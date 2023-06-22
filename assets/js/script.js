3e34645967a4c756ca9e47807e8ec317c56304c0
J_D_Nye <garwoodjdm@gmail.com>
2023-06-23 02:09:17 +1000

    colour coding of schedule added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,69 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 var date_display = $('#currentDay')
 date_display.text(dayjs().format("dddd, MMMM D"))
+
+var NINE = $('#hour-9')
+NINE.attr('style', 'background-color: ')
+var TEN = $('#hour-10')
+var ELEVEN = $('#hour-11')
+var TWELVE = $('#hour-12')
+var ONE = $('#hour-13')
+var TWO = $('#hour-14')
+var THREE = $('#hour-15')
+var FOUR = $('#hour-16')
+var FIVE = $('#hour-17')
+
+// 9am Code
+
+function PPF(timeblock, digit) {
+  var hour = moment().hours()
+  hour = hour + 13
+  if ((hour - digit)>0) {
+    timeblock.attr('style', 'background-color: grey')
+  } else if ((hour - digit)==0) {
+    timeblock.attr('style', 'background-color: red')
+  } else {
+    timeblock.attr('style', 'background-color: lime')
+  }
+}
+
+PPF(NINE, 9)
+PPF(TEN, 10)
+PPF(ELEVEN, 11)
+PPF(TWELVE, 12)
+PPF(ONE, 13)
+PPF(TWO, 14)
+PPF(THREE, 15)
+PPF(FOUR, 16)
+PPF(FIVE, 17)
+
+
+
+/*
+var hour = moment().hours()
+if ((hour - 9)>0) {
+  NINE.attr('style', 'background-color: grey')
+} else if ((hour - 9)==0) {
+  NINE.attr('style', 'background-color: red')
+} else {
+  NINE.attr('style', 'background-color: lime')
+}
+
+
+$(function() {
+  PPF = function(timeblock, digit) {
+    var hour = moment().hours()
+    if ((hour - digit)>0) {
+      timeblock.attr('style', 'background-color: grey')
+    } else if ((hour - digit)==0) {
+      timeblock.attr('style', 'background-color: red')
+    } else {
+      timeblock.attr('style', 'background-color: lime')
+    }
+  }
+})
+
+PPF(NINE, 9)
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
@@ -23,3 +86,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+*/
