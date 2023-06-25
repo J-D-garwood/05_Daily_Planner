5bb737b83573ce761169b75cd05afb6c075e9d40
J_D_Nye <garwoodjdm@gmail.com>
2023-06-25 11:18:42 +1000

    comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+//code that displays current date at the top of the page
 var date_display = $('#currentDay')
 date_display.text(dayjs().format("dddd, MMMM D"))
-/*
-agendaNine = localStorage.getItem("nine")
-agendaTen = localStorage.getItem("ten")
-agendaEleven = localStorage.getItem("eleven")
-agendaTwelve = localStorage.getItem("twelve")
-agendaOne = localStorage.getItem("one")
-agendaTwo = localStorage.getItem("two")
-agendaThree = localStorage.getItem("three")
-agendaFour = localStorage.getItem("four")
-agendaFive = localStorage.getItem("five")
-*/
+
+//list of local storage names
 var storageLs = ['nine', 'ten', 'eleven', 'twelve', 'one', 'two', 'three', 'four', 'five']
 
+//collecting event elements
 var NINE = $('#hour-9')
 var TEN = $('#hour-10')
 var ELEVEN = $('#hour-11')
@@ -24,16 +16,20 @@ var THREE = $('#hour-15')
 var FOUR = $('#hour-16')
 var FIVE = $('#hour-17')
 
+//listed event elements
 var HOURS = [NINE, TEN, ELEVEN, TWELVE, ONE, TWO, THREE, FOUR, FIVE]
 
+//function to set saved events to their respective time blocks
 function returnSavdTxt(hour, store) {
   hour.children("textarea").text(localStorage.getItem(store))
 } 
 
+//loop to implement above function to all timeblocks
 for (i=0;i<HOURS.length;i++) {
   returnSavdTxt(HOURS[i], storageLs[i])
 }
 
+//function to color code events based on whether they are in the past, present or future
 function PPF(timeblock, digit) {
   var hour = dayjs().hour()
   if ((hour - digit)>0) {
@@ -45,11 +41,12 @@ function PPF(timeblock, digit) {
   }
 }
 
+//loop to implement above function to all timeblocks
 for (i=0;i<HOURS.length;i++) {
   PPF(HOURS[i], (i+9))
 }
 
-
+//function to save contents of text area to local storage element
 function save_btn(hour, store) {
 hour.children("button").on("click", function(event) {
   event.preventDefault()
@@ -58,38 +55,7 @@ hour.children("button").on("click", function(event) {
 })
 }
 
+//loop to implement above function to all timeblocks
 for (i=0;i<HOURS.length;i++) {
   save_btn(HOURS[i], storageLs[i])
 }
-//$("divs").children("textarea").attr("style", "text-align: center;")
-
-//console.log(NINE.children("textarea").text())
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-/*
-$(function () {
-  x = $("divs").children("textarea").text()
-  console.log(x)
-})
-*/
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //DONE
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  //DONE
